ab292972ab844cdf454026d37c21c666d4d2ae55
19960620vV <vlados117@gmail.com>
2026-03-24 09:11:13 +0300

    header desctop

--- src/pages/zero/zeroPage.vue
+++ src/pages/zero/zeroPage.vue
@@ -8,14 +8,17 @@
                 <img src="@/assets/images/logo.svg" alt="logo">
             </div>
             <div class="navbar__links">
+                <a href="#">Landing</a>
+                <a href="#">Company</a>
+                <a href="#">CMS Pages</a>
+                <a href="#">Utility</a>
             </div>
         </div>
 
         <div class="navbar__actions">
-            <div class="navbar__sale-button">
-            </div>
-            <div class="navbar__buttons">
-            </div>
+            <div class="navbar__buy-button">$129 Buy Now</div>
+            <div class="navbar__icon-button navbar__icon-button--search"><img class="navbar__icon" src="@/assets/images/search.png" alt="search"></div>
+            <div class="navbar__icon-button navbar__icon-button--cart"><img class="navbar__icon" src="@/assets/images/cart.png" alt="cart"></div>
         </div>
     </div>
 </template>
